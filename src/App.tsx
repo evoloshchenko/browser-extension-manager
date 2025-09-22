14aedddd052a260bef18bb80bf93a52cf2457225
Eugenia Voloshchenko <j.voloshchenko@gmail.com>
2025-09-22 20:54:24 +0200

    feat: setup vite, react and tailwind css

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="max-w-7xl mx-auto py-6">
+        <h1 className="text-2xl font-light underline">
+          Browser Extensions manager
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
